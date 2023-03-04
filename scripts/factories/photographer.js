a3f15eddc8e309823193ed4aadc3498cc09e7a9d
delor sebastien <wolfyxkms@gmail.com>
2023-03-04 11:22:15 +0100

    03/03/23

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,35 +1,35 @@
+/* eslint-disable no-unused-vars */
 
 function photographerFactory(data) {
-    const { id, price, country, city, tagline, name, portrait }  = data;
-    
-    const picture = `assets/photographers/${portrait}`;
+  const { id, price, country, city, tagline, name, portrait } = data;
 
-    function getUserCardDOM() {
-        const article = document.createElement('article');
-        article.addEventListener('click', function () {
-            localStorage.data = JSON.stringify(data) ;
-            document.location.href = `photographer.html?id=${id}`;
-        })
-        const img = document.createElement('img');
-        img.setAttribute("src", picture)
-        const h2 = document.createElement('h2');
-        h2.textContent = name
-        const strong = document.createElement('strong')
-        strong.textContent = city + ", " + country
-        const p = document.createElement('p')
-        p.textContent = ' " ' + tagline + ' " '
+  const picture = `assets/photographers/${portrait}`;
 
-        const span = document.createElement('span')
-        span.textContent = price + "€/jour"
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    article.addEventListener("click", function () {
+      localStorage.data = JSON.stringify(data);
+      document.location.href = `photographer.html?id=${id}`;
+    });
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const strong = document.createElement("strong");
+    strong.textContent = city + ", " + country;
+    const p = document.createElement("p");
+    p.textContent = ' " ' + tagline + ' " ';
 
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(strong)
-        article.appendChild(p)
-        article.appendChild(span)
-        return (article);
+    const span = document.createElement("span");
+    span.textContent = price + "€/jour";
 
-    }
-    localStorage.removeItem("data");
-    return { name, picture, getUserCardDOM }
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(strong);
+    article.appendChild(p);
+    article.appendChild(span);
+    return article;
+  }
+  localStorage.removeItem("data");
+  return { name, picture, getUserCardDOM };
 }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,35 +1,38 @@
+/* eslint-disable no-unused-vars */
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.setAttribute('class', 'modalForm')
-    modal.style.display = "block";
-
+  const modal = document.getElementById("contact_modal");
+  modal.setAttribute("class", "modalForm");
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
 }
 
-let sendForm = document.querySelector(".FormContact")
+let sendForm = document.querySelector(".FormContact");
 
 sendForm.addEventListener("submit", function (e) {
-    e.preventDefault()
-    let inputPrenom = document.querySelector(".Prenom")
-    let inputNom = document.querySelector(".Nom")
-    let inputEmail = document.querySelector(".Email")
-    let inputMessage = document.querySelector(".Message")
+  e.preventDefault();
+  let inputPrenom = document.querySelector(".Prenom");
+  let inputNom = document.querySelector(".Nom");
+  let inputEmail = document.querySelector(".Email");
+  let inputMessage = document.querySelector(".Message");
 
-    if (inputPrenom.value &&
-        inputNom.value &&
-        inputEmail.value &&
-        inputMessage.value) {
-            
-        e.preventDefault()
-        let responseInput = [inputPrenom.value,
-        inputNom.value,
-        inputEmail.value,
-        inputMessage.value]
+  if (
+    inputPrenom.value &&
+    inputNom.value &&
+    inputEmail.value &&
+    inputMessage.value
+  ) {
+    e.preventDefault();
+    let responseInput = [
+      inputPrenom.value,
+      inputNom.value,
+      inputEmail.value,
+      inputMessage.value,
+    ];
 
-        console.log(responseInput)
-    }
-})
+    console.log(responseInput);
+  }
+});
